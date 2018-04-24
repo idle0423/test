1412ea0ad9f16ca170e7ebba08f2a2ba99703f72
idle0423 <idle0423@126.com>
2018-04-24 17:33:03 +0800

    add test16.py

---
+++ test16/test16.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+'''
+File: test16.py
+Project: test16
+File Created: Monday, 23rd April 2018 4:26:56 pm
+Author: <<dpj>> (<<idle0423@126.com>>)
+'''
+
+import pygame
+import sys
+import random
+
+# pygame.init()
+# screen = pygame.display.set_mode([640, 480])
+# screen.fill([255, 255, 255])
+# # 画圆
+# # pygame.draw.circle(screen, [0, 0, 255], [320, 240], 30, 0)
+
+# # 画矩形，rect()矩形参数是个列表
+# # pygame.draw.rect(screen, [0, 0, 255], [250, 150, 300, 200], 0)
+
+# # 画矩形，rect()矩形参数是个列表变量
+# # my_list = [250, 150, 300, 200]
+# # pygame.draw.rect(screen, [0, 0, 255], my_list, 0)
+
+# # 画矩形，rect()矩形参数是个矩形对象变量
+# my_rect = pygame.Rect(250, 150, 300, 200)
+# pygame.draw.rect(screen, [0, 0, 255], my_rect, 2)
+# pygame.display.flip()
+# running = True
+# while running:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running =False
+
+# pygame.quit()
+
+pygame.init()
+screen = pygame.display.set_mode([640, 480])
+screen.fill([255, 255, 255])
